a7c9b58dfd913dea84a43c1172d47c69371877e6
chiahn <a222056666@gmail.com>
2020-03-09 09:38:20 +0800

    xiuyun

--- gift/models.py
+++ gift/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-class item(models.Model):
-    nav1 = models.CharField(max_length=20)
-    nav2 = models.CharField(max_length=20)
-    nav3 = models.CharField(max_length=20)
-    nav4 = models.CharField(max_length=20)
+
 
 # Create your models here.
--- gift/views.py
+++ gift/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import item
+#from xiuyun import forms
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def homepage(request):
+    return render(request,"index.html",locals())
