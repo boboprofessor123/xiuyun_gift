3e1bcc6c98257d0da0122241e13da80a991a7ce6
chiahn <a222056666@gmail.com>
2020-03-10 00:35:25 +0800

    xiuyun

--- gift/views.py
+++ gift/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import item
+#from .models import item
 #from xiuyun import forms
 from datetime import datetime
 from django.contrib.auth.models import User
